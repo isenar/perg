fec96f629c0d9b9c000283a4022764f2e2c78a46
Dariusz Dajka <dariusz.dajka@gmail.com>
2022-06-28 13:46:21 +0200

    Add recursive dir handling

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -6,13 +6,8 @@ use args::Args;
 
 use clap::Parser;
 
-use std::error::Error;
-
-fn main() -> Result<(), Box<dyn Error>> {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
-
-    println!("Args: {args:?}");
-
     let path = args.path.unwrap_or_else(|| ".".into());
     let search_results = search(args.pattern, path)?;
 
